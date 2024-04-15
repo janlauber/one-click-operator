6fe0160065539c1d1cb97f6882da214bd02db469
Jan Lauber <jan.lauber@protonmail.ch>
2024-04-15 22:26:22 +0200

    feat: Refactor service.go
    Signed-off-by: Jan Lauber <jan.lauber@protonmail.ch>

--- controllers/service.go
+++ controllers/service.go
@@ -18,65 +18,47 @@ import (
 func (r *RolloutReconciler) reconcileService(ctx context.Context, f *oneclickiov1alpha1.Rollout) error {
 	log := log.FromContext(ctx)
 
-	// Keep track of services that should exist according to the Rollout spec
 	expectedServices := make(map[string]bool)
 	for _, intf := range f.Spec.Interfaces {
+		serviceName := f.Name + "-" + intf.Name + "-svc"
+		expectedServices[serviceName] = true
 
-		expectedServices[f.Name+"-"+intf.Name+"-svc"] = true
-		// Process each interface
 		service := r.serviceForRollout(f, intf)
-
 		foundService := &corev1.Service{}
-		err := r.Get(ctx, types.NamespacedName{Name: service.Name, Namespace: f.Namespace}, foundService)
+		err := r.Get(ctx, types.NamespacedName{Name: serviceName, Namespace: f.Namespace}, foundService)
 		if err != nil && errors.IsNotFound(err) {
-			// If the Service is not found, create a new one
-			err = r.Create(ctx, service)
-			if err != nil {
-				// Handle creation error
-				r.Recorder.Eventf(f, corev1.EventTypeWarning, "CreationFailed", "Failed to create Service %s", service.Name)
+			if err := r.Create(ctx, service); err != nil {
+				r.Recorder.Eventf(f, corev1.EventTypeWarning, "CreationFailed", "Failed to create Service %s", serviceName)
 				return err
 			}
-			r.Recorder.Eventf(f, corev1.EventTypeNormal, "Created", "Created Service %s", service.Name)
+			r.Recorder.Eventf(f, corev1.EventTypeNormal, "Created", "Created Service %s", serviceName)
 		} else if err != nil {
-			// Handle other errors
-			r.Recorder.Eventf(f, corev1.EventTypeWarning, "GetFailed", "Failed to get Service %s", service.Name)
+			r.Recorder.Eventf(f, corev1.EventTypeWarning, "GetFailed", "Failed to get Service %s", serviceName)
 			return err
-		} else {
-			// If the Service exists, check if it needs to be updated
-			desiredPorts := getServicePorts(intf)
-			if !reflect.DeepEqual(foundService.Spec.Ports, desiredPorts) {
-				foundService.Spec.Ports = desiredPorts
-				err = r.Update(ctx, foundService)
-				if err != nil {
-					// Handle update error
-					r.Recorder.Eventf(f, corev1.EventTypeWarning, "UpdateFailed", "Failed to update Service %s", foundService.Name)
-					return err
-				}
-				r.Recorder.Eventf(f, corev1.EventTypeNormal, "Updated", "Updated Service %s", foundService.Name)
+		} else if !reflect.DeepEqual(foundService.Spec.Ports, getServicePorts(intf)) {
+			foundService.Spec.Ports = getServicePorts(intf)
+			if err := r.Update(ctx, foundService); err != nil {
+				r.Recorder.Eventf(f, corev1.EventTypeWarning, "UpdateFailed", "Failed to update Service %s", serviceName)
+				return err
 			}
+			r.Recorder.Eventf(f, corev1.EventTypeNormal, "Updated", "Updated Service %s", serviceName)
 		}
 	}
 
-	// Delete services that are no longer specified in the Rollout spec
 	serviceList := &corev1.ServiceList{}
 	listOpts := []client.ListOption{client.InNamespace(f.Namespace)}
-	err := r.List(ctx, serviceList, listOpts...)
-	if err != nil {
+	if err := r.List(ctx, serviceList, listOpts...); err != nil {
 		log.Error(err, "Failed to list services", "Namespace", f.Namespace)
 		return err
 	}
 
 	for _, service := range serviceList.Items {
-		if _, exists := expectedServices[service.Name]; !exists {
-			// Service is no longer needed, delete it
-			if service.Labels["one-click.dev/projectId"] == f.Namespace && service.Labels["one-click.dev/deploymentId"] == f.Name {
-				err = r.Delete(ctx, &service)
-				if err != nil {
-					r.Recorder.Eventf(f, corev1.EventTypeWarning, "DeletionFailed", "Failed to delete Service %s", service.Name)
-					return err
-				}
-				r.Recorder.Eventf(f, corev1.EventTypeNormal, "Deleted", "Deleted Service %s", service.Name)
+		if _, exists := expectedServices[service.Name]; !exists && service.Labels["one-click.dev/projectId"] == f.Namespace && service.Labels["one-click.dev/deploymentId"] == f.Name {
+			if err := r.Delete(ctx, &service); err != nil {
+				r.Recorder.Eventf(f, corev1.EventTypeWarning, "DeletionFailed", "Failed to delete Service %s", service.Name)
+				return err
 			}
+			r.Recorder.Eventf(f, corev1.EventTypeNormal, "Deleted", "Deleted Service %s", service.Name)
 		}
 	}
 
@@ -88,24 +70,19 @@ func (r *RolloutReconciler) serviceForRollout(f *oneclickiov1alpha1.Rollout, int
 		"one-click.dev/projectId":    f.Namespace,
 		"one-click.dev/deploymentId": f.Name,
 	}
-	selectorLabels := map[string]string{
-		"one-click.dev/projectId":    f.Namespace,
-		"one-click.dev/deploymentId": f.Name,
-	}
 	svc := &corev1.Service{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      f.Name + "-" + intf.Name + "-svc", // Create a unique name for the Service
+			Name:      f.Name + "-" + intf.Name + "-svc",
 			Namespace: f.Namespace,
 			Labels:    labels,
 		},
 		Spec: corev1.ServiceSpec{
-			Selector: selectorLabels,
+			Selector: labels,
 			Ports:    getServicePorts(intf),
-			Type:     corev1.ServiceTypeClusterIP, // Default to ClusterIP, modify if needed
+			Type:     corev1.ServiceTypeClusterIP,
 		},
 	}
 
-	// Set Rollout instance as the owner and controller
 	ctrl.SetControllerReference(f, svc, r.Scheme)
 	return svc
 }
@@ -116,7 +93,7 @@ func getServicePorts(intf oneclickiov1alpha1.InterfaceSpec) []corev1.ServicePort
 			Name:       intf.Name,
 			Port:       intf.Port,
 			TargetPort: intstr.FromInt(int(intf.Port)),
-			Protocol:   corev1.ProtocolTCP, // Defaulting to TCP, change as needed
+			Protocol:   corev1.ProtocolTCP,
 		},
 	}
 }
